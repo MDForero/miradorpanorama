a00861ed8545ca86fcaee8c6d8f370a0a4b2668d
MDForero <mforeroroldan@gmail.com>
2022-11-18 16:10:04 -0500

    servicios actualizados y estilos aplicados las fotografias ya redirigen al restaurante y al apartado de gampling

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -14,18 +14,26 @@ const Services = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
       </main>
-      <section>
+      <section className='center'>
+        <div id="servicios">
+          <Link to="/glamping">
 
-        <Link to="/glamping">
-          <figure>
-            <img src={glamping} width="200px" />
-          </figure>
-        </Link>
-        <Link to="/restaurante">
-          <figure>
-            <img src={restaurante} alt="imagen de restaurante que redirecciona a " width="200px" />
-          </figure>
-        </Link>
+            <figure>
+              <img src={glamping} width="400px" />
+              <center>
+                <figcaption>Toca la imagen</figcaption>
+              </center>
+            </figure>
+          </Link>
+          <Link to="/restaurante">
+            <figure>
+              <img src={restaurante} alt="" width="400px" />
+              <center>
+                <figcaption>Toca la imagen</figcaption>
+              </center>
+            </figure>
+          </Link>
+        </div>
 
       </section>
     </div>
