b39971a513d695f098ae5e2e77f79f9dde33bbe7
Michael Forero <mforeroroldan@gmail.com>
2022-12-06 16:19:30 -0500

    responsive design implementado

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, NavLink, Outlet } from 'react-router-dom'
 import Footer from './Footer'
 
 const Layout = () => {
@@ -13,9 +13,9 @@ const Layout = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link ><Link to="/">Inicio</Link></Nav.Link>
-                            <Nav.Link ><Link to="nosotros">Nosotros</Link></Nav.Link>
-                            <Nav.Link ><Link to="servicios">Servicios</Link></Nav.Link>
+                            <NavLink className="nav-link" to="/">Inicio</NavLink>
+                            <NavLink className="nav-link" to="nosotros">Nosotros</NavLink>
+                            <NavLink className="nav-link" to="servicios">Servicios</NavLink>
 
                         </Nav>
                     </Navbar.Collapse>
--- src/pages/Aboutus.js
+++ src/pages/Aboutus.js
@@ -12,14 +12,14 @@ const AboutUs = () => {
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
         </div>
       </main>
-      <section style={{textAlign:"justify"}}>
+      <section style={{ textAlign: "justify" }}>
 
         <article className="platos">
           <figure>
             <img src={mision} width="500px" />
           </figure>
           <div className='info'>
-            <h1 style={{textAlign:"center"}}>Vision</h1>
+            <h1 style={{ textAlign: "center" }}>Vision</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
           </div>
         </article>
@@ -30,82 +30,187 @@ const AboutUs = () => {
           </figure>
 
           <div className='info'>
-            <h1 style={{textAlign:"center"}}>Misión</h1>
+            <h1 style={{ textAlign: "center" }}>Misión</h1>
             <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
           </div>
         </article>
 
       </section>
-      <section className='center container'>
-        <div className='block'>
-          <div className='inline block'>
+      {/* <section className='center cards'>
 
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faMartiniGlass} size="3x" /></span>
-              <div>
-                <center>
-                  <h3 >Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </center>
-              </div>
-            </article>
-
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faMountain} size="3x" /></span>
-              <div>
-                <center>
-                  <h3>Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </center>
-              </div>
-            </article>
-
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faPlateWheat} size="3x" /></span>
-              <div>
-                <center>
-                  <h3>Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </center>
-              </div>
-            </article>
-
+        <article className='cardicon block '>
+          <span><FontAwesomeIcon icon={faMartiniGlass} size="3x" /></span>
+          <div>
+            <center>
+              <h3 >Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
           </div>
-          <div className='inline block'>
+        </article>
 
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faGlassCheers} size="3x" /></span>
-              <center>
-                <div>
-                  <h3>Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+        <article className='cardicon block '>
+          <span><FontAwesomeIcon icon={faMountain} size="3x" /></span>
+          <div>
+            <center>
+              <h3>Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
+          </div>
+        </article>
+
+        <article className='cardicon block '>
+          <span><FontAwesomeIcon icon={faPlateWheat} size="3x" /></span>
+          <div>
+            <center>
+              <h3>Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
+          </div>
+        </article>
+
+
+        <article className='cardicon block '>
+          <div style={{ background: "gray", position: "absolute" }}>
+
+            <span style={{ background: "gray" }}><FontAwesomeIcon icon={faGlassCheers} size="3x" /></span>
+          </div>
+          <div>
+            <center>
+              <h3>Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
+          </div>
+        </article>
+
+        <article className='cardicon block '>
+          <span><FontAwesomeIcon icon={faUtensils} size="3x" /></span>
+          <div>
+            <center>
+              <h3>Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
+          </div>
+        </article>
+
+        <article className='cardicon block '>
+          <span><FontAwesomeIcon icon={faHotel} size="3x" /></span>
+          <div>
+            <center>
+              <h3>Pruebas</h3>
+              <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
+            </center>
+          </div>
+        </article>
+
+      </section> */}
+      <section>
+        <div className='container-card'>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faHotel} /></span>
                 </div>
-              </center>
-            </article>
-
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faUtensils} size="3x" /></span>
-              <div>
-                <center>
-                  <h3>Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </center>
               </div>
-            </article>
-
-            <article className='cardicon block '>
-              <span><FontAwesomeIcon icon={faHotel} size="3x" /></span>
-              <div>
-                <center>
-                  <h3>Pruebas</h3>
-                  <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
-                </center>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://www.linkedin.com/in/adamdipinto/" target="_blank">_adamdipinto</a>
+                </h3>
+                <p>This is where I network and build my professional protfolio.</p>
               </div>
-            </article>
-
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faGlassCheers} /></span>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://twitter.com/AdamDipinto" target="_blank">@AdamDipinto</a>
+                </h3>
+                <p>This is where I read news and network with different social groups.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faMartiniGlass} /></span>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://github.com/atom888" target="_blank">atom888</a>
+                </h3>
+                <p>This is where I share code and work on projects.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faMountain} /></span>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://www.linkedin.com/in/adamdipinto/" target="_blank">_adamdipinto</a>
+                </h3>
+                <p>This is where I network and build my professional protfolio.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faPlateWheat} /></span>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://twitter.com/AdamDipinto" target="_blank">@AdamDipinto</a>
+                </h3>
+                <p>This is where I read news and network with different social groups.</p>
+              </div>
+            </div>
+          </div>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <div class="icon">
+                  <span className="i"><FontAwesomeIcon icon={faUtensils} /></span>
+                </div>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <h3>
+                  <a href="https://github.com/atom888" target="_blank">atom888</a>
+                </h3>
+                <p>This is where I share code and work on projects.</p>
+              </div>
+            </div>
           </div>
         </div>
       </section>
+
+
     </div>
   )
 }
