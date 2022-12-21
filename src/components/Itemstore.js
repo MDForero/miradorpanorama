88b4bb80abd19976b721f78575a1ae85976160a8
Michael Forero <mforeroroldan@gmail.com>
2022-12-21 18:05:43 -0500

    nueva variante de menu

--- src/components/Itemstore.js
+++ src/components/Itemstore.js
@@ -4,30 +4,19 @@ import { Button, Card } from 'react-bootstrap'
 const Itemstore = ({ data }) => {
     const { nombre, descripcion, valor, img } = data
     return (
-        <Card>
-            <Card.Header>
-                <Card.Title>{nombre}</Card.Title>
-            </Card.Header>
-            <Card.Body style={{ display: "grid", justifyContent: "space-around", gridTemplateColumns:"1fr 1fr 1fr", gap:"10px" }}>
-                <div style={{width:"100px"}}>
-                    <Card.Img src={img} variant="top" style={{ objectFit: "cover", width:"100%" }} />
-                </div>
-                <Card.Text>
-                    <p>{descripcion}</p>
-                </Card.Text>
-                <div>
-
-                    {typeof valor === "object" ? valor.map(({ descripcion, value }) => <div style={{ display: "flex", justifyContent: "space-between" }}>
-                        <p>{descripcion}</p>
-                        <Button variant="danger">{value}</Button>
-                    </div>
-                    ) :
-                        <div>
-                            <Button variant="danger">{valor}</Button>
-                        </div>}
-                </div>
-            </Card.Body>
-        </Card>
+        <article className='item-menu' style={{textAlign:"center"}}>
+            <figure>
+                <img src={img} width="100%" height="100%" alt="" style={{objectFit:"cover"}}/>
+            </figure>
+            <h3>{nombre}</h3>
+            <p>{descripcion}</p>
+            <div className='precios'>
+                {typeof valor === "object" ? valor.map((element)=><>
+                <p>{element.descripcion}</p>
+                <button className='btn btn-outline-danger'>{element.value}</button>
+                </>) : <button className='btn btn-outline-danger' style={{justifySelf:"center"}}>{valor}</button>}
+            </div>
+        </article>
     )
 }
 
--- src/pages/servicios/restaurant/Menu.js
+++ src/pages/servicios/restaurant/Menu.js
@@ -1,39 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, CardGroup, Container } from 'react-bootstrap'
 import Footer from '../../../components/Footer'
 import Itemstore from '../../../components/Itemstore'
 import data from "../../../images/menu"
 
 const Menu = () => {
+  const [menu, setMenu]=useState("")
   return (
     <div>
       <section className=''>
-
-        <div className='category' id="entradas">
-        </div>
-        <div className="wrapper">
-          {data.map((element) => "entradas" === element.categoria ? <Itemstore data={element} /> : null)}
-        </div>
-      </section>
-      <section>
         <div className='category' id="sandiwch">
         </div>
+        <button className='btn btn-primary' onClick={()=>setMenu("entradas")}>Entradas</button>
+        <button className='btn btn-primary' onClick={()=>setMenu("patacones")}>Encocados</button>
+        <button className='btn btn-primary' onClick={()=>setMenu("especiales")}>Especiales</button>
+        <button className='btn btn-primary' onClick={()=>setMenu("")}>Todo</button>
+       
         <div className="wrapper">
-          {data.map((element) => "sandiwch" === element.categoria ? <Itemstore data={element} /> : null)}
-        </div>
-      </section>
-      <section>
-        <div className='category' id="patacones">
-        </div>
-        <div className="wrapper">
-          {data.map((element) => "patacones" === element.categoria ? <Itemstore data={element} /> : null)}
-        </div>
-      </section>
-      <section>
-        <div className='category' id="especiales">
-        </div>
-        <div className="wrapper">
-          {data.map((element) => "especiales" === element.categoria ? <Itemstore data={element} /> : null)}
+          {data.map((element) => menu ? menu === element.categoria ? <Itemstore data={element} /> : null : <Itemstore data={element} />)}
         </div>
       </section>
     </div>
